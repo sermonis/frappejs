690aa1a6a160795d17044a0e12db88111c09c191
woodell <dev@sermonis.ru>
2025-02-11 19:12:16 +0300

    package cli workflow

--- cli.js
+++ cli.js
@@ -3,8 +3,17 @@
 const program = require('commander');
 const process = require('process');
 const package = require('./package.json');
-// const boilerplate = require('frappejs/model/boilerplate');
-const boilerplate = require('./model/boilerplate');
+const boilerplate = require('frappejs/model/boilerplate');
+// const boilerplate = require('./model/boilerplate');
+
+console.log( 'frappejs', 'cli' );
+
+// program
+//     .command('new-app <name>')
+//     .description('Create a new app in the `apps` folder')
+//     .action((name) => {
+//         boilerplate.make_app_files(name);
+//     });
 
 program
     .version(package.version)
